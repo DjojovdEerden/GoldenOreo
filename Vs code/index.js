cca67971c77b02222c57d6d79d124b477eb72041
Nasheedthegreat <z10052004@gmail.com>
2023-11-07 10:53:32 +0100

    a
    a

--- Vs code/index.js
+++ Vs code/index.js
@@ -4,6 +4,25 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16 // 1024
 canvas.height = 64 * 9 // 576
 
+class Sprite { 
+    constructor({positon}){
+        this.positon = positon
+        this.image=  new Image()
+        this.image.src = './img/backgroundLevel1.png'
+
+    }
+    draw(){
+        c.drawImage(this.image, this.positon.x, this.positon.y) 
+    }
+}
+
+const backgroundLevel1 = new Sprite({
+    positon:{  
+        x: 0,
+        y: 0,
+    },
+})
+
 const player = new Player()
 
 const keys = {
@@ -22,7 +41,7 @@ function animate() {
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
 
-
+    backgroundLevel1.draw()
     player.velocity.x = 0 // snelheid
     if (keys.d.pressed) 
         player.velocity.x = 5
